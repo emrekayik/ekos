5fd21ea33cc87bb05dbd26f5c1b38d39a36cff8c
emrekayik <emrekayik1905@gmail.com>
2024-09-07 18:16:38 +0300

    ekos v0.4 double fault

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,22 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let version = "0.3.0";
+    let version = "0.4.0";
     println!("ekos {}", version);
+
+    ekos::init();
+
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    }
+
+    // uncomment line below to trigger a stack overflow
+    // stack_overflow();
 
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
